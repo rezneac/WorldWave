05dfdd44ff69005ff0841e23edabe94d56b8983a
rezneac <rezneacanatolie@gmail.com>
2023-03-02 12:01:29 +0000

    Creating mainScreen for radio stations and testing functionality for broadcasting radio.

---
+++ src/screen/MainScreen.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button, Text, View, StyleSheet } from "react-native";
+import { Audio } from "expo-av";
+
+const MainScreen = () => {
+  const sound = new Audio.Sound();
+
+  async function playSound() {
+    try {
+      await sound.loadAsync({ uri: "https://stream.hitfm.md:8443/HitFM" });
+      await sound.playAsync();
+    } catch (error) {
+      console.log("Error loading or playing sound", error);
+    }
+  }
+
+  async function stopSound() {
+    try {
+      await sound.stopAsync();
+      await sound.unloadAsync();
+    } catch (error) {
+      console.log("Error stopping or unloading sound", error);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Start" onPress={() => playSound()} />
+      <Button title="Stop" onPress={() => stopSound()} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default MainScreen;
