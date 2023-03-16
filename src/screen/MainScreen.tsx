0fc6543f93d2882152a461dbc9c3c1a401aeb258
rezneac <rezneacanatolie@gmail.com>
2023-03-16 10:43:14 +0000

    Fixing playing and stoping radio broadcast

--- src/screen/MainScreen.tsx
+++ src/screen/MainScreen.tsx
@@ -1,43 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, FlatList, TouchableOpacity } from "react-native";
 import { Audio } from "expo-av";
 import RadioStation from "../components/radioStation";
 
 const MainScreen = () => {
+  const [SwitchMode, SetSwitchMode] = useState(false);
   const sound = new Audio.Sound();
 
   const radioStation = require("../content/radio_info.json");
 
-  async function playSound(uri: string) {
+  async function manageSound(uri: string) {
     try {
-      await sound.loadAsync({ uri });
-      await sound.playAsync();
+      if (!sound._loaded) {
+        await sound.loadAsync({ uri });
+        await sound.playAsync();
+      } else {
+        await sound.stopAsync();
+        await sound.unloadAsync();
+      }
     } catch (error) {
       console.log("Error loading or playing sound", error);
     }
   }
 
-  async function stopSound() {
-    try {
-      await sound.stopAsync();
-      await sound.unloadAsync();
-    } catch (error) {
-      console.log("Error stopping or unloading sound", error);
-    }
-  }
-
   return (
     <View style={styles.container}>
-
       {/* Displays list of radio stations */}
       <FlatList
         data={radioStation}
         keyExtractor={(item) => item.stationName}
         numColumns={3}
+        initialNumToRender={50}
         renderItem={({ item, index }) => {
           return (
             <TouchableOpacity
-              onPress={() => playSound(radioStation[index].url)}
+              onPress={() => manageSound(radioStation[index].url)}
             >
               <View style={styles.radioStationView}>
                 <RadioStation details={radioStation} index={index} />
@@ -53,7 +50,6 @@ const MainScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
